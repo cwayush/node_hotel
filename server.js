1b9cf16a5492215680eab58f7c8453848ce817a0
Ayush <imrealayushthakur@gmail.com>
2024-12-10 16:52:15 +0530

    Connect mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', function (req, res) {
     res.send('Welcome to my Hotel... How i can help you ?')
@@ -11,11 +14,11 @@ app.get('/', function (req, res) {
 
 
 const personroutes = require('./routes/personrouts')
-app.use('/person', personroutes)
-
 const menuitems = require('./routes/menuitem')
+
+app.use('/person', personroutes)
 app.use('/MenuItem', menuitems)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Listening on port 3000')
 });
